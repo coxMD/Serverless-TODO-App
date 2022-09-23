ec1b3f702ac05d54438465198df5fc43eb83c49d
coxMD <coxmusyoki@gmail.com>
2022-09-23 20:56:44 +0300

    mofified config.ts file

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '';
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+const apiId = 'lbkvif47gl'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
 
 export const authConfig = {
-  domain: '',
-  clientId: '',
+  domain: 'dev-o04euceu.us.auth0.com',
+  clientId: 'MePZeMbBv81LVHuLDTJAeXNtM8PSiF7E',
   callbackUrl: 'http://localhost:3000/callback'
-};
+}
